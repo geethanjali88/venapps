5c337762807144e513836fb61ccef98f650854c2
SRI <SRI@SRI-PC>
2017-06-19 16:18:57 +0530

    1 st file change

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -2,30 +2,18 @@ package com.app;
 
 public class Employee {
 	private int empId;
-	private String empName;
-	private double empSal;
-	public double getEmpSal() {
-		return empSal;
-	}
-	public void setEmpSal(double empSal) {
-		this.empSal = empSal;
-	}
+	
+	
 	public int getEmpId() {
 		return empId;
 	}
 	public void setEmpId(int empId) {
 		this.empId = empId;
 	}
-	public String getEmpName() {
-		return empName;
-	}
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
+	
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName
-				+ ", empSal=" + empSal + "]";
+		return "Employee [empId=" + empId + "]";
 	}
 	
 	
